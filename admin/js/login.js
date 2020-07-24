5e8b72a770666b389c19e8dd6f8b466f53b838f6
wJinDou <478956917@qq.com>
2020-07-24 11:41:32 +0800

    初始化页面+登录验证

---
+++ admin/js/login.js
@@ -0,0 +1,30 @@
+var input_txt = $(".input_txt");
+var input_pass = $(".input_pass");
+var input_sub = $(".input_sub");
+var from = $(".login_form");
+input_sub.click(function (e) {
+  //阻止表单内按钮和submit会触发的默认提交方式
+  e.preventDefault();
+
+  if (input_txt.val().trim() === "" || input_pass.val().trim() === "") {
+    $("#myModal").find("#modaltText").text("用户名或密码不能为空");
+    $("#myModal").modal("show");
+    return;
+  }
+  $.ajax({
+    url: "http://localhost:8080/api/v1/admin/user/login",
+    type: "post",
+    data: from.serialize(),
+    success: (info) => {
+      // console.log(info);
+      $("#myModal").find("#modaltText").text(info.msg);
+      $("#myModal").modal("show");
+      if (info.code === 200) {
+        $("#myModal").on("hide.bs.modal", function () {
+          localStorage.setItem("token", info.token);
+          location.href = "./index.html";
+        });
+      }
+    },
+  });
+});
